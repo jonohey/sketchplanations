9a12fc39a20c36543b75968a3e6034f4b865faea
Alistair Holt <hello@alistairholt.co.uk>
2020-09-27 18:37:24 +0100

    Add infinite scrolling to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Prismic from 'prismic-javascript'
-import Link from 'next/link'
 import { client } from 'prismic-configuration'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { Sketchplanation, Navigation } from 'components'
 
 const Home = ({ sketchplanations }) => {
+  const [page, setPage] = useState(2)
+  const [hasMore, setHasMore] = useState(true)
+  const [results, setResults] = useState(sketchplanations.results)
+
+  const fetchMore = async () => {
+    const moreSketchplanations = await client.query(Prismic.Predicates.at('document.type', 'sketchplanation'), {
+      orderings: '[my.sketchplanation.published_at desc]',
+      page,
+      pageSize: 5,
+    })
+    setResults([...results, ...moreSketchplanations.results])
+    setPage(page + 1)
+    if (page === moreSketchplanations.total_pages) setHasMore(false)
+  }
+
   return (
     <div>
       <div className='masthead'>
@@ -12,14 +27,50 @@ const Home = ({ sketchplanations }) => {
         <p className='sm:text-lg'>Explaining one thing a week in a sketch</p>
         <Navigation />
       </div>
-      <div className='sketchplanations'>
-        {sketchplanations.results.map((sketchplanation) => (
-          <Sketchplanation key={sketchplanation.uid} sketchplanation={sketchplanation} />
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={results.length}
+        next={fetchMore}
+        hasMore={hasMore}
+        loader={
+          <div className='loading'>
+            Loading more…{' '}
+            <svg viewBox='0 0 38 38' xmlns='http://www.w3.org/2000/svg' stroke='#000'>
+              <g transform='translate(1 1)' strokeWidth='2' fill='none' fillRule='evenodd'>
+                <path d='M36 18c0-9.94-8.06-18-18-18'>
+                  <animateTransform
+                    attributeName='transform'
+                    type='rotate'
+                    from='0 18 18'
+                    to='360 18 18'
+                    dur='1s'
+                    repeatCount='indefinite'
+                  />
+                </path>
+              </g>
+            </svg>
+          </div>
+        }
+      >
+        <div className='sketchplanations'>
+          {results.map((sketchplanation) => (
+            <Sketchplanation key={sketchplanation.uid} sketchplanation={sketchplanation} />
+          ))}
+        </div>
+      </InfiniteScroll>
       <style jsx>{`
         nav a:hover {
           text-decoration: underline;
+        }
+
+        .loading {
+          @apply flex mx-auto pb-20 items-center justify-center;
+          max-width: 460px;
+        }
+
+        .loading svg {
+          @apply ml-4;
+          width: 20px;
+          height: 20px;
         }
       `}</style>
     </div>
@@ -29,7 +80,7 @@ const Home = ({ sketchplanations }) => {
 Home.getInitialProps = async () => {
   const sketchplanations = await client.query(Prismic.Predicates.at('document.type', 'sketchplanation'), {
     orderings: '[my.sketchplanation.published_at desc]',
-    pageSize: 100,
+    pageSize: 5,
   })
 
   return { sketchplanations }
