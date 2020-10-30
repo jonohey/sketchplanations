14363a6d9ca5ad4c54a274875281f70660a013f1
Alistair Holt <hello@alistairholt.co.uk>
2020-10-30 21:05:30 +0000

    Squash complaint from Next about a missing 404 page

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Error = () => {
+  return <img className='block mx-auto mt-8' src='/error.png' alt='Error' width={640} height={542.5} />
+}
+
+export default Error
