13b4f90a6b0d605b15e729f05a21dfa15c1e13d5
Alistair Holt <hello@alistairholt.co.uk>
2021-01-17 13:42:38 +0000

    Refine

--- components/PrevNextSketchplanation.js
+++ components/PrevNextSketchplanation.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import Imgix from 'react-imgix'
+import { useHotkeys } from 'react-hotkeys-hook'
 
 const PrevNextSketchplanation = ({ sketchplanation, kind }) => {
+  const keyboardKey = kind === 'next' ? 'j' : 'k'
+  const router = useRouter()
+
   if (!sketchplanation) return null
+
+  useHotkeys(keyboardKey, () => router.push(`/${sketchplanation?.uid}`))
 
   const {
     data: { image, title },
     uid,
   } = sketchplanation
 
-  const label = kind === 'previous' ? 'Previous' : 'Next'
-
   return (
     <>
-      {/* <pre>{JSON.stringify(sketchplanation)}</pre> */}
       <Link href={`/${uid}`}>
         <a className='image'>
+          <div className='caret-wrapper'>
+            <svg
+              className='caret'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+              fillRule='evenodd'
+              clipRule='evenodd'
+            >
+              <path d='M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z' />
+            </svg>
+          </div>
           <Imgix
             className='lazyload'
             src={image.url}
@@ -36,26 +51,35 @@ const PrevNextSketchplanation = ({ sketchplanation, kind }) => {
             sizes='(min-width: 648px) 600px, (min-width: 640px) calc(100vw - 3rem), 100w'
           />
           <div className='info'>
-            <span className='label'>{label}</span>
             <span className='title'>{title}</span>
+            <div className='keyboard-shortcuts'>
+              <kbd className='keyboard-key'>{keyboardKey}</kbd>
+            </div>
           </div>
         </a>
       </Link>
       <style jsx>{`
         .image {
-          @apply flex p-4 items-center border rounded-lg overflow-hidden shadow;
-          flex-direction: ${kind === 'previous' ? 'row' : 'row-reverse'};
-          text-align: ${kind === 'previous' ? 'left' : 'right'};
+          @apply p-2 flex items-center border rounded-lg overflow-hidden shadow;
+          flex-direction: ${kind === 'next' ? 'row' : 'row-reverse'};
+          text-align: ${kind === 'next' ? 'left' : 'right'};
         }
 
         .image :global(img) {
-          @apply flex-none;
-          width: 38%;
+          @apply flex-none m-2;
+          width: 20%;
           max-width: 140px;
         }
 
+        @screen sm {
+          .image :global(img) {
+            width: 38%;
+          }
+        }
+
         .info {
-          @apply px-4;
+          @apply items-center px-2;
+          flex-direction: ${kind === 'previous' ? 'row' : 'row-reverse'};
         }
 
         .label {
@@ -63,7 +87,34 @@ const PrevNextSketchplanation = ({ sketchplanation, kind }) => {
         }
 
         .title {
-          @apply block text-lg font-semibold;
+          @apply block font-semibold;
+        }
+
+        @screen sm {
+          .title {
+            @apply text-lg;
+          }
+        }
+
+        .caret-wrapper {
+          @apply flex-none m-2;
+          width: 1rem;
+          transform: ${kind === 'next' ? 'rotate(180deg)' : 'none'};
+        }
+
+        .caret * {
+          fill: #000;
+        }
+
+        .keyboard-shortcuts {
+          @apply mt-2;
+        }
+
+        .keyboard-key {
+          @apply inline-flex items-center justify-center border rounded text-sm font-sans uppercase;
+          width: 1.5rem;
+          height: 1.5rem;
+          color: #a9b1ba;
         }
       `}</style>
     </>
