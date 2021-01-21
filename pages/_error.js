6e9aca618939e27d7fb550bc63d320f69683ed6f
jonohey <jono@zeneducate.com>
2021-01-21 16:02:37 +0000

    Added tags link to error page

--- pages/_error.js
+++ pages/_error.js
@@ -7,7 +7,7 @@ const Error = () => {
       <div className='content'>
         <img className='block mx-auto mt-8' src='/error.png' alt='Error' width={640} height={542.5} />
         <p className='contact-me text-center mx-auto mt-1 px-10'>
-          Maybe try to <a href='/search'>search Sketchplanations</a>? Please let me know if a link is wrong so I can fix it for others. I’m
+          Maybe <a href='/search'>search Sketchplanations</a> or try looking in <a href='/tags'>tags</a>? Please let me know if a link is wrong so I can fix it for others. I’m
           at <a href='mailto:jono.hey@gmail.com'>jono.hey@gmail.com</a>
         </p>
       </div>
