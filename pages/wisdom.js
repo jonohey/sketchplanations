1d221a26decf5c00a7ef096fec9779947e71dab6
jonohey <jono@zeneducate.com>
2021-03-25 19:08:50 +0000

    added wisdom page

---
+++ pages/wisdom.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Page } from 'components'
+import { client } from 'prismic-configuration'
+
+const Privacy = ({ document }) => {
+  return <Page document={document} />
+}
+
+Privacy.getInitialProps = async () => {
+  const document = await client.getSingle('wisdom')
+  return { document }
+}
+
+export default Privacy
