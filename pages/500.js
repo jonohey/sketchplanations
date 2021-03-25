857a35f545237918b67a4e16ebbc54abdc433b8a
Alistair Holt <hello@alistairholt.co.uk>
2021-03-25 19:59:58 +0000

    Add 500 page

---
+++ pages/500.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='root'>
+      <div className='content'>
+        <img className='block mx-auto mt-8' src='/error.png' alt='Error' width={640} height={542.5} />
+        <p className='contact-me text-center mx-auto mt-1 px-10'>
+          Maybe <a href='/search'>search Sketchplanations</a> or try looking in <a href='/tags'>tags</a>? Please let me
+          know if a link is wrong so I can fix it for others. I’m at{' '}
+          <a href='mailto:jono.hey@gmail.com'>jono.hey@gmail.com</a>
+        </p>
+      </div>
+      <style jsx>{`
+        .contact-me {
+          @apply flex flex-col items-center;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Error
