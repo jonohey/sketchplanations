b8cb41d53c09176473b70cc8e6758f0ae14d75ea
jonohey <jono@zeneducate.com>
2021-03-25 21:26:48 +0000

    fixed styling on error pages

--- pages/500.js
+++ pages/500.js
@@ -4,16 +4,29 @@ const Error = () => {
   return (
     <div className='root'>
       <div className='content'>
-        <img className='block mx-auto mt-8' src='/error.png' alt='Error' width={640} height={542.5} />
-        <p className='contact-me text-center mx-auto mt-1 px-10'>
-          Maybe <a href='/search'>search Sketchplanations</a> or try looking in <a href='/tags'>tags</a>? Please let me
-          know if a link is wrong so I can fix it for others. I’m at{' '}
-          <a href='mailto:jono.hey@gmail.com'>jono.hey@gmail.com</a>
+        <img className='oops block mx-auto mt-4' src='/error.png' alt='Error' width={640} height={542.5} />
+        <p className='help-text text-center mx-auto mt-1 px-10'>
+          Something broke. Sorry! Maybe <a href='/search'>search Sketchplanations</a> or try looking in <a href='/tags'>tags</a>?
+        </p>
+        <p className='help-text text-center mx-auto mt-1 px-10'>
+          Please let me know this page is broken so I can fix it. I’m at{' '}
+          <a href='mailto:jono.hey@gmail.com?subject=Broken link to fix!'>jono.hey@gmail.com</a>
         </p>
       </div>
       <style jsx>{`
-        .contact-me {
-          @apply flex flex-col items-center;
+        
+        .help-text {
+          max-width: 600px;
+          width: 100%;
+          margin: 0 auto 1.5em;
+        }
+
+        a:hover {
+          text-decoration: underline;
+        }
+
+        a {
+          @apply text-blue;
         }
       `}</style>
     </div>
