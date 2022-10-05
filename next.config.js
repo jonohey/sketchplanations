3d5d6792c8890dc119d1091dd5deace265bedb04
Alistair Holt <hello@alistairholt.co.uk>
2022-10-05 22:48:30 +0100

    Remove config we don’t need

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 module.exports = {
-  // i18n: {
-  //   locales: ['en-GB'],
-  //   defaultLocale: 'en-GB',
-  // },
-  images: {
-    loader: 'imgix',
-    domains: ['images.prismic.io'],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        request: 'empty',
-      }
-    }
-
-    return config
-  },
   async headers() {
     return [
       {
