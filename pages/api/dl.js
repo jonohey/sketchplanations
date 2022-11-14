9c88e336a2ac37b58745a4d38d2017031f2bcfbc
Alistair Holt <hello@alistairholt.co.uk>
2022-11-14 17:43:01 +0000

    Fix download endpoint

--- pages/api/dl.js
+++ pages/api/dl.js
@@ -1,17 +1,9 @@
 const request = require('request')
-const Prismic = require('@prismicio/client')
-export const apiEndpoint = 'https://sketchplanations.prismic.io/api/v2'
-
-const initApi = (req) => {
-  return Prismic.getApi(apiEndpoint, {
-    req: req,
-  })
-}
+const { client } = require('utils/helpers')
 
 export default async (req, res) => {
   const uid = req.query.uid
-  const api = await initApi(req)
-  const sketchplanation = await api.getByUID('sketchplanation', uid, { fetch: 'sketchplanation.image' })
+  const sketchplanation = await client.getByUID('sketchplanation', uid, { fetch: 'sketchplanation.image' })
 
   const imageUrl = sketchplanation.data.image.url.split('?')[0]
   const extension = imageUrl.split('.').pop().toLowerCase()
