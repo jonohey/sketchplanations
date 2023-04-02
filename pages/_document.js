bb05cce09ea4f84fdc478d2e008abd0c528791b0
Alistair Holt <hello@alistairholt.co.uk>
2023-04-02 16:54:40 +0100

    Add preconnect for Imgix (#212)

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,7 @@ class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
           <link rel='preconnect' href='https://js.stripe.com' crossOrigin='true' />
+          <link rel='preconnect' href='https://images.prismic.io' />
           <link href='https://fonts.googleapis.com/css2?family=Inter:wght@300;600&display=swap' rel='stylesheet' />
           <link
             rel='stylesheet'
