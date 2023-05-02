1a41af2fbddb52c40ca159338554e1f1bd345e27
Alistair Holt <hello@alistairholt.co.uk>
2023-05-02 02:07:33 +0100

    Move secret token auth

--- pages/api/random.js
+++ pages/api/random.js
@@ -8,16 +8,6 @@ const sql = postgres({
 })
 
 export default async (req, res) => {
-  // Verify secret parameter exists
-  if (!req.query.secret) {
-    return res.status(401).send('Unauthorized')
-  }
-
-  // Verify secret parameter is correct
-  if (req.query.secret !== process.env.CRON_SECRET) {
-    return res.status(401).send('Unauthorized')
-  }
-
   const result = await sql`SELECT * FROM sketchplanations ORDER BY RANDOM() LIMIT 1`
 
   res.redirect(`/${result[0].handle}`)
