8c06b6ff1c24e3f2d4277c19ae03ff56a5f7719b
Alistair Holt <hello@alistairholt.co.uk>
2023-05-10 22:11:39 +0100

    Restore scroll position when returning home (#229)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
-import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 import LoadingIndicator from 'components/LoadingIndicator'
@@ -16,7 +17,7 @@ import styles from './index.module.css'
 const Sketchplanation = dynamic(() => import('../components/Sketchplanation'))
 
 const Home = ({ sketchplanations: initialSketchplanations }) => {
-  const [page, setPage] = useState(2)
+  const [page, setPage] = useState(3) // 3 because we already have 8 sketchplanations
   const [hasMore, setHasMore] = useState(true)
   const [sketchplanations, setSketchplanations] = useState(initialSketchplanations)
 
@@ -33,10 +34,68 @@ const Home = ({ sketchplanations: initialSketchplanations }) => {
         pageSize: 4,
       })
     ).results
-    setSketchplanations([...sketchplanations, ...moreSketchplanations])
-    setPage(page + 1)
+
+    const nextSketchplanations = [...sketchplanations, ...moreSketchplanations]
+    const nextPage = page + 1
+
+    setSketchplanations(nextSketchplanations)
+    setPage(nextPage)
+
+    sessionStorage.setItem('sketchplanations', JSON.stringify(nextSketchplanations))
+    sessionStorage.setItem('page', JSON.stringify(nextPage))
     if (page === moreSketchplanations.total_pages) setHasMore(false)
   }
+
+  const manualScrollRestoration = typeof window !== 'undefined' && 'scrollRestoration' in window.history
+
+  useEffect(() => {
+    if (!manualScrollRestoration) return
+
+    window.history.scrollRestoration = 'manual'
+
+    return () => {
+      if (!manualScrollRestoration) return
+
+      window.history.scrollRestoration = 'auto'
+    }
+  }, [])
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const savedSketchplanations = JSON.parse(sessionStorage.getItem('sketchplanations'))
+    const savedPage = JSON.parse(sessionStorage.getItem('page'))
+
+    if (savedSketchplanations && savedPage) {
+      setSketchplanations(savedSketchplanations)
+      setPage(savedPage)
+    }
+
+    const handleRouteChangeComplete = (url) => {
+      if (url !== '/') return
+
+      const savedScrollY = JSON.parse(sessionStorage.getItem('scrollY'))
+      if (!savedScrollY) return
+
+      window.scrollTo(0, savedScrollY)
+      sessionStorage.removeItem('scrollY')
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChangeComplete)
+
+    const handleRouteChangeStart = (url) => {
+      if (url === '/') return
+
+      sessionStorage.setItem('scrollY', JSON.stringify(window.scrollY))
+    }
+
+    router.events.on('routeChangeStart', handleRouteChangeStart)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart)
+      router.events.off('routeChangeComplete', handleRouteChangeComplete)
+    }
+  }, [])
 
   return (
     <>
@@ -74,18 +133,18 @@ const Home = ({ sketchplanations: initialSketchplanations }) => {
   )
 }
 
-Home.getInitialProps = async () => {
+export const getStaticProps = async () => {
   const sketchplanations = (
     await client.getByType('sketchplanation', {
       orderings: {
         field: 'my.sketchplanation.published_at',
         direction: 'desc',
       },
-      pageSize: 4,
+      pageSize: 8,
     })
   ).results
 
-  return { sketchplanations }
+  return { props: { sketchplanations } }
 }
 
 export default Home
