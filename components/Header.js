9cdd8343e3f8086ce566e21d2ed7ef278e815677
Jono Hey <jono.hey@gmail.com>
2024-07-01 13:51:23 +0100

    Made the logo scroll to the top on the homepage and still be a link on others (#354)

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,38 @@
 import Link from 'next/link'
-
-import Navigation from 'components/Navigation'
+import { useRouter } from 'next/router'
 
 import styles from './Header.module.css'
 
+import Navigation from 'components/Navigation'
+
+const LogoPicture = () => (
+  <picture>
+    <source srcSet='/logo-small.svg 100w' media='(max-width: 549px)' />
+    <source srcSet='/logo.svg 100w' media='(min-width: 550px)' />
+    <img src='/logo.svg' className={styles.ident__svg} alt='Sketchplanations' />
+  </picture>
+)
+
 const Header = () => {
+  const router = useRouter()
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
   return (
     <div className={styles.root}>
-      <Link href='/' className={styles.ident}>
-        <picture>
-          <source srcSet='/logo-small.svg 100w' media='(max-width: 549px)' />
-          <source srcSet='/logo.svg 100w' media='(min-width: 550px)' />
-          <img src='/logo.svg' className={styles.ident__svg} alt='Sketchplanations' />
-        </picture>
-      </Link>
+      {router.pathname === '/' ? (
+        <div onClick={scrollToTop} className={styles.ident} style={{ cursor: 'pointer' }}>
+          <LogoPicture />
+        </div>
+      ) : (
+        <Link href='/' className={styles.ident}>
+          <LogoPicture />
+        </Link>
+      )}
       <Navigation />
     </div>
   )
