1fae25dfc8316153d86f9b584cd3778cdfca755a
Jono Hey <jono.hey@gmail.com>
2024-07-03 20:41:05 +0100

    Removed react-justified-grid and replaced with flex grid including titles for all locations (#359)

--- components/SketchplanationsGrid.js
+++ components/SketchplanationsGrid.js
@@ -1,10 +1,6 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import JustifiedGrid from 'react-justified-grid'
+import styles from './SketchplanationsGrid.module.css'
 
 import { isBlank } from 'helpers'
-
-import styles from './SketchplanationsGrid.module.css'
 
 const prismicDocsToImages = (prismicDocs) => {
   try {
@@ -25,6 +21,7 @@ const prismicDocsToImages = (prismicDocs) => {
         height,
         alt: alt || `${title} - Sketchplanations`,
         uid,
+        title,
       })
     )
   } catch {
@@ -38,28 +35,14 @@ const SketchplanationsGrid = ({ prismicDocs }) => {
   if (isBlank(images)) return <div className={styles.noResults}>No sketches found</div>
 
   return (
-    <JustifiedGrid images={images} maxRowHeight={550} gutter={20} showIncompleteRow>
-      {(processedImages) => {
-        return (
-          <>
-            {processedImages.map((image, index) => {
-              const { src, width, height, left, top, originalData } = image
-              return (
-                <Link key={index} href={`/${originalData.uid}`} className='absolute bg-paper' style={{ left, top }}>
-                  <Image
-                    src={src}
-                    width={width}
-                    height={height}
-                    alt={originalData.alt}
-                    sizes='(min-width: 2750px) calc(20vw + 20 * 4), (min-width: 2200px) 25vw, (min-width: 1650px) 33vw, (min-width: 1100px) 50vw, 100vw'
-                  />
-                </Link>
-              )
-            })}
-          </>
-        )
-      }}
-    </JustifiedGrid>
+    <div className={styles.grid}>
+      {images.map((image) => (
+        <div key={image.uid} className={styles.gridItem}>
+          <div className={styles.title}>{image.title}</div>
+          <img src={image.src} alt={image.alt} style={{ maxWidth: '100%', height: 'auto' }} />
+        </div>
+      ))}
+    </div>
   )
 }
 
