87f82656bbd493033320f248854035ba1becb850
jonohey <jono.hey@gmail.com>
2024-09-16 14:46:04 +0100

    Added Licence to footer and adjusted categories

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,13 @@
-import { ExternalLink } from "lucide-react";
-import { Cards } from "./Cards";
-import SubscribeInline from "./SubscribeInline";
-import FancyLink from "./FancyLink";
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, ExternalLink } from "lucide-react";
 import { useEffect, useState } from "react";
+import { Cards } from "./Cards";
+import FancyLink from "./FancyLink";
+import SubscribeInline from "./SubscribeInline";
 
 import styles from "./Footer.module.css";
 
 const Nav = () => (
-	<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-8 lg:text-center gap-x-12 gap-y-1 items-start">
+	<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-9 lg:text-center gap-x-12 gap-y-2 items-start">
 		<FancyLink href="/">Home</FancyLink>
 		<FancyLink href="/big-ideas-little-pictures">Book!</FancyLink>
 		<FancyLink
@@ -35,6 +34,7 @@ const Nav = () => (
 		</FancyLink>
 		<FancyLink href="/categories">Categories</FancyLink>
 		<FancyLink href="/archive">Archive</FancyLink>
+		<FancyLink href="/archive">Licence</FancyLink>
 	</div>
 );
 
@@ -52,20 +52,24 @@ const categories = [
 		uid: "cognitive-bias",
 	},
 	{
+		title: "Design",
+		uid: "design",
+	},
+	{
 		title: "Science",
 		uid: "science",
 	},
 	{
-		title: "Framework",
-		uid: "framework",
+		title: "Motivation",
+		uid: "motivation",
 	},
 	{
-		title: "Words",
-		uid: "words",
+		title: "Writing",
+		uid: "writing",
 	},
 	{
-		title: "Food and drink",
-		uid: "food-and-drink",
+		title: "Coffee",
+		uid: "coffee",
 	},
 	{
 		title: "Behavioral economics",
@@ -73,11 +77,7 @@ const categories = [
 	},
 	{
 		title: "Quotes",
-		uid: "quotes",
-	},
-	{
-		title: "Ideas",
-		uid: "ideas",
+		uid: "quote",
 	},
 	{
 		title: "Drawing",
@@ -86,14 +86,6 @@ const categories = [
 	{
 		title: "Nature",
 		uid: "nature",
-	},
-	{
-		title: "Weather",
-		uid: "weather",
-	},
-	{
-		title: "What’s the difference between",
-		uid: "whats-the-difference-between",
 	},
 ];
 
@@ -113,8 +105,7 @@ const Footer = () => {
 					<div className="mb-3 text-xl font-semibold">Explore more</div>
 					<div className="prose mb-6">
 						<p className="text-textSubdued text-balance ">
-							The sketches cover all sorts of topics. Here are some common
-							themes to find what interests you:
+							The sketches cover many topics. Here are some of my favourites:
 						</p>
 					</div>
 					<div className="columns-2 md:columns-3 lg:columns-5 -my-1">
