a9455c0fcc1b7b6c9cf8429a81d13012aaaf3d5f
jonohey <jono.hey@gmail.com>
2024-09-20 11:34:50 +0100

    Matched double quotes in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
 				ignore: ["jsx"],
 			},
 		],
-		'react/no-unescaped-entities': ['error', { forbid: ['\'', '"'] }],
+		"react/no-unescaped-entities": ['error', { forbid: ['\'', '"'] }],
 	},
 	settings: {
 		"import/core-modules": ["styled-jsx/css"],
