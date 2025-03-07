500b0ec1d394454c390cf1def0ef32c427ee173c
Jono Hey <jono.hey@gmail.com>
2025-03-07 10:06:39 +0000

    Fix hardcoded /explore and updated broken link report (#486)

--- components/Oops.js
+++ components/Oops.js
@@ -2,9 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import styles from "./Oops.module.css";
-
 import { pageTitle } from "helpers";
+import styles from "./Oops.module.css";
 
 import oopsImage from "../public/oops.png";
 
@@ -16,29 +15,36 @@ const Oops = ({ children }) => {
 			<Head>
 				<title>{pageTitle("Oops")}</title>
 			</Head>
-			<div className={styles.main}>
-				<div className={styles.image}>
+			<main className={`${styles.main} max-w-3xl mx-auto px-4 py-8 text-center prose prose-blue`}>
+				<h1 className="sr-only">Page Not Found</h1>
+				<div className="mb-8 not-prose flex justify-center">
 					<Image
 						src={oopsImage}
 						alt="Error"
 						width={640}
 						height={542.5}
 						loader={loader}
+						aria-label="404 error illustration"
+						className={styles.image}
 					/>
 				</div>
 				{children}
-				<p>
-					Maybe <Link href="/explore">search Sketchplanations</Link> or try
-					looking in <Link href="/categories">categories</Link>?
-				</p>
-				<p>
-					Please let me know if a link is broken so I can fix it for others. I’m
-					at{" "}
-					<a href="mailto:jono.hey@gmail.com?subject=Broken link to fix!">
-						jono.hey@gmail.com
-					</a>
-				</p>
-			</div>
+				<div className="space-y-6">
+					<p className="mb-4">
+						Maybe try <Link href="/search">search</Link> or <Link href="/categories">categories</Link>?
+					</p>
+					<p>
+						Please <Link href="https://forms.gle/aVeicJAyn38FDR4F8" target="_blank" rel="noopener noreferrer">report the broken link</Link> to me so I can fix it for others. Or email me
+						at{" "}
+						<a href="mailto:jono.hey@gmail.com?subject=404%20Error%20-%20Broken%20Link%20Report">
+							jono.hey@gmail.com
+						</a>
+					</p>
+					<p>
+						<Link href="/">Return to Home</Link>
+					</p>
+				</div>
+			</main>
 		</>
 	);
 };
