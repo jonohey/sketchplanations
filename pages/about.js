9a98cc7abfbb6566a378cb687ad69d0058758ce5
jonohey <jono.hey@gmail.com>
2026-01-05 11:54:50 +0000

    Added Foundling - Reprise embed to about page

--- pages/about.js
+++ pages/about.js
@@ -684,6 +684,20 @@ const About = () => {
 								loading="lazy"
 							></iframe>
 						</div>
+						
+						<div className="my-8" id="Foundling-reprise-embed">
+							<iframe
+								data-testid="embed-iframe"
+								style={{borderRadius: "12px"}}
+								src="https://open.spotify.com/embed/album/2BciIeBDGtRL3dQVqmCjSH?utm_source=generator"
+								width="100%"
+								height="352"
+								frameBorder="0"
+								allowfullscreen={true}
+								allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+								loading="lazy">
+							</iframe>
+						</div>
 
 						<div className="my-8" id="blossom-embed">
 							<iframe 
